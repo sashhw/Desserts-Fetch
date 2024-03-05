9ac58c4fbf7b59e72c9b52d216fc86dd61861b3e
swalkowski <swalkowski@foxtrotco.com>
2024-03-05 09:25:39 -0600

    Initial Commit

---
+++ Desserts-Fetch/Desserts_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desserts_FetchApp.swift
+//  Desserts-Fetch
+//
+//  Created by Sasha Walkowski on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Desserts_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
