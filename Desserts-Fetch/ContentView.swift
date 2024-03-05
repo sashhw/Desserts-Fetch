7f7cd4f2c0efbf1dfa9578956a9027b86a28ee94
swalkowski <swalkowski@foxtrotco.com>
2024-03-05 10:17:57 -0600

    created ViewModel, added networking code w/ errors, also set up content view with list of items

--- Desserts-Fetch/ContentView.swift
+++ Desserts-Fetch/ContentView.swift
@@ -8,17 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    @State private var desserts: [Dessert] = []
+    @State private var selectedDessert: Dessert?
+    @State private var dessertDetails: DessertDetails?
+    @State private var isPresented = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            HStack {
+                Text("Desserts")
+                    .font(.largeTitle)
+                    .padding(.trailing, 8)
+                Image(systemName: "birthday.cake")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 25, height: 25)
+            }
+            List {
+                ForEach(desserts, id: \.self) { dessert in
+                    HStack(alignment: .center) {
+                        AsyncImage(url: URL(string: dessert.imageURL)) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(Rectangle())
+                        } placeholder: {
+                            Rectangle()
+                                .foregroundColor(.secondary)
+                        }
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(5)
 
-#Preview {
-    ContentView()
+                        Text(dessert.name)
+                            .font(.body)
+
+                        Spacer()
+
+                        Image(systemName: "arrow.right")
+                            .frame(width: 15)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        if let id = dessert.id {
+                            selectedDessert = dessert
+                            Task {
+                                await fetchDetails(id: id)
+                            }
+                            isPresented = true
+                        }
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .onAppear {
+                Task {
+                    do {
+                        desserts = try await viewModel.fetch()
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                }
+            }
+        }
+    }
+
+    func fetchDetails(id: String) async {
+        let details =  await viewModel.fetchDetails(id: id)
+        dessertDetails = details
+    }
 }
